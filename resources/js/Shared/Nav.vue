4ef0608f9c18b93f8365ad845a701405c02fc93a
Zahid Hasan Limon <96983732+zahidupm@users.noreply.github.com>
2023-06-20 12:39:26 +0600

    active links

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,14 +1,22 @@
 <template>
     <nav class="mt-6">
         <ul class="list-disc">
-            <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
-            <li><Link href="/users" class="text-blue-500 hover:underline">Users</Link></li>
-            <li><Link href="/settings" class="text-blue-500 hover:underline">Settings</Link></li>
-            <li><Link href="/logout" :data="{foo: 'bar'}" method="POST" as="button" class="text-blue-500 hover:underline">Logout</Link></li>
-    </ul>
+            <li>
+                <NavLink href="/" :active="$page.component === 'Home'">
+                    Home</NavLink>
+            </li>
+            <li>
+                <NavLink href="/users" :active="$page.component === 'Users'">
+                    Users</NavLink>
+            </li>
+            <li>
+                <NavLink href="/settings" :active="$page.component === 'Settings'">
+                    Settings</NavLink>
+            </li>
+        </ul>
     </nav>
 </template>
 
 <script setup>
-    import { Link } from '@inertiajs/vue3';
+import NavLink from '../Shared/NavLink.vue';
 </script>
---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,12 @@
+<template>
+    <Link class="text-blue-500 hover:underline" :class="{ 'font-bold underline': active }">
+    <slot />
+    </Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+defineProps({
+    active: Boolean
+})
+</script>
